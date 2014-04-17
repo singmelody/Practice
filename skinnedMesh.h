60b2db309ec1712a955f74491bea375872823f3a
singmelody <446538645@qq.com>
2014-04-18 00:44:29 +0800

    HierarchyLoader 从 skinnedmesh 里面分离起来

--- skinnedMesh.h
+++ skinnedMesh.h
@@ -50,30 +50,6 @@ struct BoneMesh: public D3DXMESHCONTAINER
 
 };
 
-class BoneHierarchyLoader: public ID3DXAllocateHierarchy
-{
-public:
-	BoneHierarchyLoader() :frameCount(0),meshCount(0){}
-
-	STDMETHOD(CreateFrame)(THIS_ LPCSTR Name, LPD3DXFRAME *ppNewFrame);
-	STDMETHOD(DestroyFrame)(THIS_ LPD3DXFRAME pFrameToFree);
-	STDMETHOD(DestroyMeshContainer)(THIS_ LPD3DXMESHCONTAINER pMeshContainerBase);
-
-	STDMETHOD(CreateMeshContainer)(THIS_ 
-		LPCSTR Name, 
-		CONST D3DXMESHDATA *pMeshData, 
-		CONST D3DXMATERIAL *pMaterials, 
-		CONST D3DXEFFECTINSTANCE *pEffectInstances, 
-		DWORD NumMaterials, 
-		CONST DWORD *pAdjacency, 
-		LPD3DXSKININFO pSkinInfo, 
-		LPD3DXMESHCONTAINER *ppNewMeshContainer);
-
-	int frameCount;
-	int meshCount;
-};
-
-
 class SkinnedMesh
 {
 public:
