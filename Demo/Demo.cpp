d523764be5cc16a8d3048f7ce8376b6994ef3fa3
singmelody <446538645@qq.com>
2014-06-05 08:50:23 +0800

    demo sln upload

---
+++ Demo/Demo.cpp
@@ -0,0 +1,15 @@
+// Demo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+namespace Dream
+{
+	class DemoApp
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
