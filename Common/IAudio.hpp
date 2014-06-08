d4f96063a57b8515396fa30140708d2cf49729f0
singmelody <446538645@qq.com>
2014-06-08 19:03:48 +0800

    Audio Sys upload

---
+++ Common/IAudio.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Dream
+{
+
+	class IAudio
+	{
+	public:
+		IAudio(void){}
+		virtual ~IAudio(void){}
+
+		virtual bool Init() = 0;
+		virtual void Destroy() = 0;
+
+		virtual void Update(float deltaTime) = 0;
+		virtual void Render() = 0;
+	};
+
+	extern IAudio*	gRenderer;
+}
+
+#define DEFINE_AUDIO_INTERFACE  Dream::IAudio*  Dream::gIAudio = NULL;
+
+#define CREATE_AUDIO_DLL_FUNC   "CreateAudio"
+typedef void(*CreateAudioFunc)(Dream::IAudio**);
