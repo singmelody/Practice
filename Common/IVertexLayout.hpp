69e0126e8755d5166c37d43ce7c098e567fb4ab6
singmelody <446538645@qq.com>
2014-06-24 23:56:39 +0800

    add layout enum

--- Common/IVertexLayout.hpp
+++ Common/IVertexLayout.hpp
@@ -2,13 +2,34 @@
 
 namespace Dream
 {
+	typedef unsigned short	WORD;
+	typedef unsigned long	DWORD;
+	typedef unsigned char	BYTE;
+
 	class IVertexLayout
 	{
 	public:
+		enum VERTEXUSAGE
+		{
+			POSITION,
+			COLOR,
+			WEIGHT,
+			SIZE,
+
+			USAGE_COUNT
+		};
+
+		enum DATATYPE
+		{
+			FLOAT3,
+			FLOAT2,
+			DWORD
+		};
+
 		IVertexLayout(){}
 		virtual ~IVertexLayout(){}
 
-		virtual BuildSignature() = 0;
+		virtual void AddSignature(WORD streamIdx, WORD Offset, BYTE type, BYTE method, BYTE usage, BYTE usageIdx) = 0;
 
 		virtual int GetVertexSize() const { return m_vertexByteSize; }
 
