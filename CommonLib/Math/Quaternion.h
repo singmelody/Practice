6ad6d0e8da453c8203066e165d82b50c7aa35313
singmelody <446538645@qq.com>
2014-07-18 21:46:00 +0800

    math lib upload

--- CommonLib/Math/Quaternion.h
+++ CommonLib/Math/Quaternion.h
@@ -27,13 +27,39 @@ public:
 
 	Quaternion Multiply(const Quaternion& q)
 	{
-		return Quaternion( q.x * x, q.y * y, q.z * z, q.w * w);
+		x = w * q.x + x * q.w + y * q.z - z * q.y;
+		y = w * q.y + y * q.w + z * q.x - x * q.z;
+		z = w * q.z + z * q.w + x * q.y - y * q.x;
+		w = w * q.w - x * q.x - y * q.y - z * q.z;
 	}
 
-// 	Quaternion Slerp( const Quaternion& q, float t)
-// 	{
-// 		return ;
-// 	}
+	Quaternion Quaternion::operator+ (const Quaternion& rkQ) const
+	{
+		return Quaternion(w+rkQ.w,x+rkQ.x,y+rkQ.y,z+rkQ.z);
+	}
+	//-----------------------------------------------------------------------
+	Quaternion Quaternion::operator- (const Quaternion& q) const
+	{
+		return Quaternion( x - q.x , y - q.y, z - q.z, w - q.w);
+	}
+
+	Quaternion Quaternion::operator* (float value) const
+	{
+		return Quaternion(value*w,value*x,value*y,value*z);
+	}
+
+	Quaternion Quaternion::operator*( const Quaternion& q) const
+	{
+		return Quaternion(
+			w * q.x + x * q.w + y * q.z - z * q.y
+			,
+			w * q.y + y * q.w + z * q.x - x * q.z
+			,
+			w * q.z + z * q.w + x * q.y - y * q.x
+			,
+			w * q.w - x * q.x - y * q.y - z * q.z
+			);
+	}
 };
 
 }
